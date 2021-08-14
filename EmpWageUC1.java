141cb6619594b3f9f3746b304f117df615781701
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-14 20:51:06 +0530

    uc1

---
+++ EmpWageUC1.java
@@ -0,0 +1,13 @@
+
+public class EmpWageUC1 {
+	public static void main (String[] args) {
+		int A = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == A)
+			System.out.println("emp is p");
+		else
+			System.out.println("emp is A ");
+	}
+	
+
+}
